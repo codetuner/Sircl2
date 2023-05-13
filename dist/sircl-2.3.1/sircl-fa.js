d81532e728a79f2e8a2a697d47d204538bff8a62
codetuner <codetuner@users.noreply.github.com>
2023-05-13 13:07:59 +0200

    Released 2.3.1

---
+++ dist/sircl-2.3.1/sircl-fa.js
@@ -0,0 +1,5 @@
+﻿// Initialize sircl lib:
+if (typeof sircl === "undefined") console.warn("The 'sircl-fa' component should be registered after the 'sircl' component. Please review order of script files.");
+
+// Use a Font Awesome spinner:
+sircl.html_spinner = '<i class="sircl-spinner fas fa-circle-notch fa-spin"></i> ';
