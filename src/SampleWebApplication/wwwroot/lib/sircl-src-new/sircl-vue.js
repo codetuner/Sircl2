bbbcbccbd6398d817e02a7d7841483f4943d4540
codetuner <codetuner@users.noreply.github.com>
2024-05-21 08:57:10 +0200

    Vue integration: local renames

--- src/SampleWebApplication/wwwroot/lib/sircl-src-new/sircl-vue.js
+++ src/SampleWebApplication/wwwroot/lib/sircl-src-new/sircl-vue.js
@@ -7,7 +7,7 @@ if (typeof Vue === "undefined") console.warn("The 'sircl-vue' component should b
 // Sircl Vue object for extension:
 sircl.vue = sircl.vue || {};
 sircl.vue.createHandlers = sircl.vue.createHandlers || [];
-//sircl.vue.createHandlers.push(function (el, appObj) { console.log("Vue: Creating", el, appObj); });
+//sircl.vue.createHandlers.push(function (el, rootComponent) { console.log("Vue: Creating", el, appObj); });
 sircl.vue.mountHandlers = sircl.vue.createHandlers || [];
 //sircl.vue.mountHandlers.push(function (el, app) { console.log("Vue: Mounting", el, app); });
 sircl.vue.unmountHandlers = sircl.vue.createHandlers || [];
@@ -28,28 +28,28 @@ $$("before", function () {
 $$(function () {
     $(this).find("[vue-dataurl]").each(async function () {
         // Prepare VueJs app:
-        let appObject = {};
+        let rootComponent = {};
         let responseprom = (this.hasAttribute("vue-dataurl")) ? fetch(this.getAttribute("vue-dataurl")) : null;
         let templateresponseprom = (this.hasAttribute("vue-templateurl")) ? fetch(this.getAttribute("vue-templateurl")) : null;
         if (responseprom) {
             const response = await responseprom;
             const json = await response.json();
-            appObject.data = function () { return json; };
+            rootComponent.data = function () { return json; };
         }
         if (this.hasAttribute("vue-template")) {
-            appObject.template = sircl.ext.$select($(this), this.getAttribute("vue-template")).html();
+            rootComponent.template = sircl.ext.$select($(this), this.getAttribute("vue-template")).html();
         } else if (templateresponseprom) {
             const templateresponse = await templateresponseprom;
             const template = await templateresponse.text();
-            appObject.template = template;
+            rootComponent.template = template;
         }
         // Event handlers to inject Sircl Ready Handlers:
-        appObject.beforeMount = () => { sircl._beforeUnload(this); };
-        appObject.mounted = function () { sircl._afterLoad(this.$el.parentElement); };
-        sircl.vue.createHandlers.forEach((fx) => fx(this, appObject));
+        rootComponent.beforeMount = () => { sircl._beforeUnload(this); };
+        rootComponent.mounted = function () { sircl._afterLoad(this.$el.parentElement); };
+        sircl.vue.createHandlers.forEach((fx) => fx(this, rootComponent));
         // Create and mount VueJs app:
         const { createApp } = Vue;
-        const app = createApp(appObject);
+        const app = createApp(rootComponent);
         this.__vue_app = app;
         sircl.vue.mountHandlers.forEach((fx) => fx(this, app));
         app.mount(this);
